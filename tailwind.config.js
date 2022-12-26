f9488aeeac310a11602c7ac0b1b8b7a5e3e58706
a6uxa4 <a6uxa4@icloud.com>
2022-12-26 14:07:31 +0600

    save

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./**/*.html'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				mainColor: '#050c2a',
+			},
+			padding: {
+				big: '50px',
+			},
+		},
+	},
+	plugins: [],
+}
